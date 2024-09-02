763771b41e07dbe93bf9fa5e2db34009f7dc29d4
MakamuT <makamuthe2@gmail.com>
2024-09-02 21:56:43 +0000

    webpack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -11,5 +11,11 @@ function updateCounter() {
 const debouncedUpdateCounter = _.debounce(updateCounter, 300);
 
 $(document).ready(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="startButton">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
   $('#startButton').on('click', debouncedUpdateCounter);
 });
