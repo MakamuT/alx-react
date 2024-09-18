fafcae3d968392722e440f5806c8024ae77c613d
Thembisile  Makamu <makamuthe2@gmail.com>
2024-09-18 23:45:51 +0200

    Aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionMarginBottom.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionMarginBottom.js
@@ -3,6 +3,12 @@ import PropTypes from "prop-types";
 import BodySection from "./BodySection";
 import { StyleSheet, css } from "aphrodite";
 
+const styles = StyleSheet.create({
+    marginBottom: {
+        marginBottom: '40px',
+    },
+});
+
 const BodySectionWithMarginBottom = (props) => {
   return (
     <div className="bodySectionWithMargin">
